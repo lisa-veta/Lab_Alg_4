ca4e945b85368e747eb7775028dc4651982b8a9a
Elizaveta Pernikova <lisapernikowa@mail.ru>
2023-11-14 19:51:33 +0500

    correct

--- Lab_Alg_4/Models/BubbleSort.cs
+++ Lab_Alg_4/Models/BubbleSort.cs
@@ -8,23 +8,48 @@ namespace Lab_Alg_4.Models
 {
     public class BubbleSort
     {
-        public List<ItemSort> ItemsSort = new List<ItemSort>(); 
+        public List<ItemSort> ItemsSort = new List<ItemSort>();
+
+        public ItemSort ItemSort
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
         public void DoBubbleSort(List<Item> items)
         {
+            string comment = "Сортировка пузырьком\n";
+            bool flag = true;
             Item temp;
             for (int i = 0; i < items.Count - 1; i++)
             {
+                comment += $"\nРассматриваем массив от начала до {items.Count - i} элемента\n";
+                flag = true;
                 for (int j = 0; j < items.Count - i - 1; j++)
                 {
                     if (items[j].Content > items[j + 1].Content)
                     {
-                        ItemsSort.Add(new ItemSort(items[j].Id, items[j + 1].Id, Copyer.CopyListItem(items)));
+                        if (flag)
+                        {
+                            comment += $"{items[j].Content} > {items[j + 1].Content} меняем их местами";
+                            ItemsSort.Add(new ItemSort(items[j].Id, items[j + 1].Id, Copyer.CopyListItem(items), comment));
+                            flag = false;
+                        }
+                        else
+                        {
+                            comment = $"{items[j].Content} > {items[j + 1].Content} меняем их местами";
+                            ItemsSort.Add(new ItemSort(items[j].Id, items[j + 1].Id, Copyer.CopyListItem(items), comment));
+                        }
                         temp = items[j];
                         items[j] = items[j + 1];
                         items[j + 1] = temp;
                         ItemsSort.Add(new ItemSort(items[j].Id, items[j + 1].Id, Copyer.CopyListItem(items)));
+                        comment = "";
                     }
                 }
+                comment = $"\nПередвинули максимальный элемент\nна позицию {items.Count - i}\n";
             }
         }
     }
